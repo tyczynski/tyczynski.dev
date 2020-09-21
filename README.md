9641394ab2b08d77a965209ac538903f46560a82
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-09-21 17:04:19 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Personal webpage - tyczynski.dev
+
+## Getting Started
+
+Run the development server:
+
+```bash
+# Run the development server
+$ yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Scripts
+
+```bash
+# Run the development server
+$ yarn dev
+
+# Build the production version
+$ yarn build
+
+# Run builded production version
+$ yarn start
+```
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+
+const IndexPage = () => (
+  <>
+    <Head>
+      <title>tyczynski.dev</title>
+    </Head>
+
+    <main>
+      <h1>
+        Przemysław Tyczyński
+      </h1>
+
+      <h2>Front-end developer</h2>
+
+      <p >
+        Contact me: <a href="mailto:przemyslaw@tyczynski.pl">przemyslaw@tyczynski.pl</a>
+      </p>
+
+      <hr />
+      <p>Find me on:</p>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/ptyczynski/" target="_blank" rel="noopener">LinkedIn</a></li>
+        <li><a href="https://www.github.com/tyczynski/" target="_blank" rel="noopener">GitHub</a></li>
+      </ul>
+
+      <hr />
+      <p>My publications (that people who know the Polish language):</p>
+      <ul>
+        <li><a href="https://geek.justjoin.it/vue-js-3-0-nowosci-i-zmiany-co-przyniosla-nowa-wersja" target="_blank" rel="noopener">Vue.js 3.0 – nowości i zmiany. Co przyniosła nowa wersja? </a></li>
+        <li><a href="https://www.empressia.pl/blog/179-obserwowanie-zmian-rozmiaru-elementow-z-resize-observer-api" target="_blank" rel="noopener">Obserwowanie zmian rozmiaru elementów z Resize Observer API</a></li>
+      </ul>
+
+      <p style={{
+        color: 'hsl(0, 0%, 100%)'
+      }}>
+        One day I'll be a beautiful webpage! :D
+      </p>
+    </main>
+  </>
+);
+
+export default IndexPage;
